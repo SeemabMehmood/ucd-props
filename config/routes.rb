2dfbc2fa571cb88e37d9d3825b13ba28a3604ecd
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-09-24 21:16:13 +0500

    Property management done

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'property_management', to: "home#property_management"
   get 'contact_us', to: "home#contact_us"
   get 'about_us', to: "home#about_us"
   get 'sell', to: "home#sell"
