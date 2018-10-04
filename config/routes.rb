b967e4113a72a51e69c92db269914dd7eae96b6a
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-10-04 17:47:46 +0500

    Proeprties in admin added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :properties
+  end
 
   get 'property_management', to: "home#property_management"
   get 'contact_us', to: "home#contact_us"
