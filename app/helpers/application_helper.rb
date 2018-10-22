56f6e4c0c67282fbdee53c8561e33683c6c1005b
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-10-22 15:23:08 +0500

    Added links for buy rent

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,6 @@
 module ApplicationHelper
   def active_class(current_path)
+    return 'active' if controller_name == 'properties' && current_path.index(params[:type])
     'active' if current_path.index(action_name)
   end
 end
