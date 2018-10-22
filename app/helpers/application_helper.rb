1a3f0429c35b2c377120ea7be6ac4ca218722f5c
Seemab Mehmood <seemab.mehmood25@gmail.com>
2018-10-22 17:14:58 +0500

    index ready

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def active_class(current_path)
-    return 'active' if controller_name == 'properties' && current_path.index(params[:type])
+    return 'active' if controller_name == 'home' && current_path.index(controller_name)
+    return 'active' if controller_name == 'properties' && ( params[:type].present? && current_path.index(params[:type]))
     'active' if current_path.index(action_name)
   end
 end
