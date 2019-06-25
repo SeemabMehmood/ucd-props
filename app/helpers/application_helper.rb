52bd99d533962bf95dfb485177ea78d6365db026
Seemab Mehmood <seemab.mehmood25@gmail.com>
2019-06-25 17:04:42 +0500

    Fixed active nav links issue

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def active_class(current_path)
-    return 'active' if controller_name == 'home' && current_path.index(controller_name)
+    return 'active' if current_path == 'home' && request.path == '/'
     return 'active' if controller_name == 'properties' && ( params[:type].present? && current_path.index(params[:type]))
     'active' if current_path.index(action_name)
   end
