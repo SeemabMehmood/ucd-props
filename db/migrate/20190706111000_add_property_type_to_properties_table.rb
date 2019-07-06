34f835a452feb309a0fad2095eb0b5dccc88d584
Seemab Mehmood <seemab.mehmood25@gmail.com>
2019-07-06 16:12:03 +0500

    Added property type in properties table

---
+++ db/migrate/20190706111000_add_property_type_to_properties_table.rb
@@ -0,0 +1,5 @@
+class AddPropertyTypeToPropertiesTable < ActiveRecord::Migration[5.2]
+  def change
+    add_column :properties, :property_type, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_05_084544) do
+ActiveRecord::Schema.define(version: 2019_07_06_111000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2018_10_05_084544) do
     t.datetime "updated_at", null: false
     t.string "avatar"
     t.float "constructed_area", default: 0.0, null: false
+    t.string "property_type"
   end
 
 end
